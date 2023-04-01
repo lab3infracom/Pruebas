3bfdb3d3c32ce2498c4aac3585bd5246713c63df
Javier Cerino <j.cerino@uniandes.edu.co>
2023-03-31 23:00:23 -0500

    First Commit

---
+++ src/Cliente.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class Cliente {
+
+    public final static int PUERTO = 42869;
+
+    private static String ip = "192.168.20.35";
+
+    public static void main(String[] args) {
+
+        try {
+
+            DatagramSocket socketUDP = new DatagramSocket();
+            InetAddress ipServidor = InetAddress.getByName(ip);
+ 
+
+            byte[] buffer = new byte[1024];
+            
+            String mensaje = "Hola como estas? Que tal el clima por allá";
+
+            buffer = mensaje.getBytes();
+
+            DatagramPacket peticion = new DatagramPacket(buffer, buffer.length, ipServidor, PUERTO);
+
+            try {
+                socketUDP.send(peticion);
+            } catch (IOException e) {
+                System.out.println("Error al enviar el mensaje");
+            }
+
+            System.out.println("Se envia el mensaje al servidor");
+
+            DatagramPacket respuesta = new DatagramPacket(buffer, buffer.length);
+
+            try {
+                socketUDP.receive(respuesta);
+            } catch (IOException e) {
+                System.out.println("Error al recibir el mensaje");
+            }
+
+            System.out.println("Se recibe la respuesta del servidor"+ new String(respuesta.getData()));
+
+        } catch (SocketException e) {
+            System.out.println("Error al crear el socket");
+        } catch (UnknownHostException e) {
+            System.out.println("Error al crear el socket");
+        }
+
+    }
+    
+}
---
+++ src/ServidorUDP.java
@@ -0,0 +1,48 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class ServidorUDP {
+
+    public final static int PUERTO = 42869;
+
+    public static void main(String[] args) throws Exception {
+        
+        byte[] buffer = new byte[1024];
+
+        try{ 
+
+            System.out.println("Inicio del servidor UDP");
+            DatagramSocket socket = new DatagramSocket(PUERTO);
+
+            DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
+            socket.receive(peticion);
+            System.out.println("Se recibe un mensaje de conexión");
+
+            int puertoCliente = peticion.getPort();
+            InetAddress IpCliente = peticion.getAddress();
+
+            String mensaje = new String(peticion.getData());
+            mensaje = "Recibido el mensaje del cliente: " + mensaje;
+            buffer = mensaje.getBytes();
+                        
+            DatagramPacket respuesta = new DatagramPacket(buffer, buffer.length, IpCliente, puertoCliente);
+
+            socket.send(respuesta);
+
+            System.out.println("Se manda el mensaje de confirmación. (NO es TCP, no buscamos hacer Acknowledge)");
+
+            // while(true){
+            // }
+
+        } catch (SocketException ex) {
+            System.out.println("Error al crear el socket");
+        }
+
+
+        
+        System.out.println("Hello, World!");
+    }
+}
